91bb9e21d72765c7f969956d4ef6407772d89377
Francisco Ferioli Marco <franciscoferiolimarco@protonmail.com>
2020-04-13 03:13:14 -0300

    Cambios varios
    Título, descripción y autores del sitio.

--- sitio/index.php
+++ sitio/index.php
@@ -1,17 +1,17 @@
 <?php
 
-	/*if (  isset($_POST['user_latlng'])  ) {
-		$user_latlng			= $_POST['user_latlng']; // "lat lng"
-	} elseif (  isset($_GET['user_latlng'])  ) {
-		$user_latlng			= $_GET['user_latlng'];
-	}
+    /*if (  isset($_POST['user_latlng'])  ) {
+        $user_latlng            = $_POST['user_latlng']; // "lat lng"
+    } elseif (  isset($_GET['user_latlng'])  ) {
+        $user_latlng            = $_GET['user_latlng'];
+    }
 
 
-	if (  !empty($user_latlng) && (strlen($user_latlng) > 1 )  )
-	{
-		echo $user_latlng;  
-		die();
-	}*/
+    if (  !empty($user_latlng) && (strlen($user_latlng) > 1 )  )
+    {
+        echo $user_latlng;  
+        die();
+    }*/
 
 
 
@@ -36,12 +36,12 @@ require_once('custom/scripts/funciones-db.php');
 <!-- meta pal' face -->
 <meta property="og:url" content="http://www.arboladourbano.com/" />
 <meta property="og:title" content="Mapa del Arbolado Urbano" />
-<meta property="og:description" content="Buscador de árboles para la Ciudad de Buenos Aires. Datos recolectados en el censo de arbolado realizado por el GCBA entre el año 2011 y 2014. El objetivo principal de este sitio es simplificar el acceso a esta valiosa información esperando que colabore con el conocimiento y el cuidado de nuestro arbolado urbano." />
+<meta property="og:description" content="Buscador de árboles para todas las ciudades del mundo. Datos obtenidos de los censos forestales publicados en los portales de datos abiertos de las municipalidades, y de colaboradores independientes. El objetivo principal de este sitio es simplificar el acceso a esta valiosa información esperando que colabore con el conocimiento y el cuidado de nuestro arbolado urbano." />
 <meta property="og:image" content="https://arboladourbano.com.ar/images/logo-152x152.png" />
 
-<title>Arbolado Urbano - árboles de la Ciudad Autónoma de Buenos Aires</title>
-<meta name="description" content="Buscador de árboles para la Ciudad de Buenos Aires. Datos recolectados en el censo de arbolado realizado por el GCBA entre el año 2011 y 2014.">
-<meta name="author" content="Martín Simonyan">
+<title>Arbolado Urbano - árboles de todas las ciudades del mundo</title>
+<meta name="description" content="Buscador de árboles para todas las ciudades del mundo. Datos obtenidos de los censos forestales publicados en los portales de datos abiertos de las municipalidades, y de colaboradores independientes.">
+<meta name="author" content="Martín Simonyan & Francisco Ferioli Marco">
 
 <!-- /ht Andy Clarke - http://front.ie/lkCwyf -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
@@ -88,7 +88,7 @@ require_once('custom/scripts/funciones-db.php');
 
 /*echo("CONSULTA: <br>" . $busqueda);
 if ( isset($censo_query)) {
-	echo "<div>QUERY: <br><br><pre>$censo_query</pre></div>";	
+    echo "<div>QUERY: <br><br><pre>$censo_query</pre></div>";    
 }*/
 //echo "<br><br><div>POST : <br><br><pre>". print_r($_POST) ."</pre></div>";
 
@@ -96,19 +96,19 @@ if ( isset($censo_query)) {
 //// Parámetro para ver ID especie
 
 if ( isset($_GET['ver_especie_id'])  ) {
-	$_SESSION['ver_especie_id'] = $_GET['ver_especie_id'];
+    $_SESSION['ver_especie_id'] = $_GET['ver_especie_id'];
 
-	if($_GET['ver_especie_id'] == 0) {
-		unset($_SESSION["ver_especie_id"]); 
-		session_destroy();
-	}
+    if($_GET['ver_especie_id'] == 0) {
+        unset($_SESSION["ver_especie_id"]); 
+        session_destroy();
+    }
 }
 
 if (isset($_SESSION['ver_especie_id'])) {
-	echo "<div id='consola'><pre>";
-	echo "¡Hola voluntario!<br>";
-	echo "ver especie id activado";
-	echo "</pre></div>";
+    echo "<div id='consola'><pre>";
+    echo "¡Hola voluntario!<br>";
+    echo "ver especie id activado";
+    echo "</pre></div>";
 }
 /// fin ver parametro ID especie
 
@@ -116,94 +116,94 @@ if (isset($_SESSION['ver_especie_id'])) {
 //// Parámetro para ver DEBUG
 
 if ( isset($_GET['debug'])  ) {
-	$_SESSION['debug'] = $_GET['debug'];
+    $_SESSION['debug'] = $_GET['debug'];
 
-	if($_GET['debug'] == 0) {
-		unset($_SESSION["debug"]); 
-		session_destroy();
-	}
+    if($_GET['debug'] == 0) {
+        unset($_SESSION["debug"]); 
+        session_destroy();
+    }
 }
 
 if (isset($_SESSION['debug'])) {
-	echo "<div id='consola' class='debug'><pre>";
-	echo "MODO DEBUG<br>";
-	echo $busqueda . "<br>---<br>";
-	if (isset($censo_query)) echo $censo_query;
-	echo "</pre></div>";
+    echo "<div id='consola' class='debug'><pre>";
+    echo "MODO DEBUG<br>";
+    echo $busqueda . "<br>---<br>";
+    if (isset($censo_query)) echo $censo_query;
+    echo "</pre></div>";
 }
 /// fin ver parametro DEBUG
 
 ?>
 
 <nav class="navbar navbar-default navbar-fixed-bottom visible-sm visible-xs">
-	<div class="container-fluid">
-		<a class="btn btn-default navbar-btn scroll" href="#busca_arboles"><i class="fa fa-search fa-sm"></i> Buscador <i class="fa fa-caret-up fa-sm"></i> </a>
-		<a class="btn btn-default navbar-btn scroll" href="#mapa"><i class="fa fa-map-marker fa-sm"></i> Mapa <i class="fa fa-caret-down fa-sm"></i></a>
-	</div>
+    <div class="container-fluid">
+        <a class="btn btn-default navbar-btn scroll" href="#busca_arboles"><i class="fa fa-search fa-sm"></i> Buscador <i class="fa fa-caret-up fa-sm"></i> </a>
+        <a class="btn btn-default navbar-btn scroll" href="#mapa"><i class="fa fa-map-marker fa-sm"></i> Mapa <i class="fa fa-caret-down fa-sm"></i></a>
+    </div>
 </nav>
 <div class="container-fluid full-height">
-	<div class="row full-height">
-		<div class="progress navbar-fixed-bottom">
-			<div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100" style="width: 0%"> </div>
-		</div>
-		
-		<!-- modal arbol (container) -->
-		<div class="col-xs-12 col-sm-9 col-md-6" id="info-arbol"> </div>
+    <div class="row full-height">
+        <div class="progress navbar-fixed-bottom">
+            <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100" style="width: 0%"> </div>
+        </div>
+        
+        <!-- modal arbol (container) -->
+        <div class="col-xs-12 col-sm-9 col-md-6" id="info-arbol"> </div>
 
-		<!-- left slide bar -->
-		<div class="col-md-4 col-lg-3" id="menu">
-			<nav>
-				<a class="title" href="<?php echo $APP_URL; ?>/">
-					<h1>
-						Arbolado Urbano
-						<small>Buenos Aires</small>
-					</h1>
-					<img src="images/logo-152x152-blanco.png" alt="Arbolado Urbano">
-				</a>
-				<?php require_once 'custom/scripts/form.php'; ?>
+        <!-- left slide bar -->
+        <div class="col-md-4 col-lg-3" id="menu">
+            <nav>
+                <a class="title" href="<?php echo $APP_URL; ?>/">
+                    <h1>
+                        Arbolado Urbano
+                        <small>Internacional</small>
+                    </h1>
+                    <img src="images/logo-152x152-blanco.png" alt="Arbolado Urbano">
+                </a>
+                <?php require_once 'custom/scripts/form.php'; ?>
 
-				
-				<div class="row red">
-					<div class="col-xs-12 col-sm-3 col-md-6">
-						<a class="btn btn-default btn-small btn-block facebook" href="https://www.facebook.com/arboladomapa" target="_blank"><i class="fa fw fa-facebook-official"></i> Seguinos</a></div>
-					<div class="col-xs-12 col-sm-3 col-md-6">
-						<button class="btn btn-default btn-small btn-block que-es-esto" data-toggle="modal" data-target="#que-es-esto">Sobre el mapa</button></div>	
-				</div>
+                
+                <div class="row red">
+                    <div class="col-xs-12 col-sm-3 col-md-6">
+                        <a class="btn btn-default btn-small btn-block facebook" href="https://www.facebook.com/arboladomapa" target="_blank"><i class="fa fw fa-facebook-official"></i> Seguinos</a></div>
+                    <div class="col-xs-12 col-sm-3 col-md-6">
+                        <button class="btn btn-default btn-small btn-block Qué-es-esto?" data-toggle="modal" data-target="#Qué-es-esto?">Sobre el mapa</button></div>    
+                </div>
 
-				<div id="adsense" class="row">
-					<div class="col-xs-12">
-						<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-						<!-- arbolado1 -->
-						<ins class="adsbygoogle"
-						     style="display:block"
-						     data-ad-client="ca-pub-7228206495347837"
-						     data-ad-slot="8591261973"
-						     data-ad-format="auto"></ins>
-						<script>
-						(adsbygoogle = window.adsbygoogle || []).push({});
-						</script>
-					</div>
-				</div>
+                <div id="adsense" class="row">
+                    <div class="col-xs-12">
+                        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                        <!-- arbolado1 -->
+                        <ins class="adsbygoogle"
+                             style="display:block"
+                             data-ad-client="ca-pub-7228206495347837"
+                             data-ad-slot="8591261973"
+                             data-ad-format="auto"></ins>
+                        <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                    </div>
+                </div>
 
-				
-				<div class="red red2 row">
-					<div class="col-xs-12">	
-						<p class="col-xs-12 col-sm-3 col-md-12 este-mapa">Este mapa cuenta con<br> la valiosa colaboración de:</p>
+                
+                <div class="red red2 row">
+                    <div class="col-xs-12">    
+                        <p class="col-xs-12 col-sm-3 col-md-12 este-mapa">Este mapa cuenta con<br> la valiosa colaboración de:</p>
 
-						<a class="col-xs-6 col-sm-3 col-md-6 col-lg-4 lcnrs" href="https://www.facebook.com/LaCiudadNosRegalaSabores" target="_blank"><img src="<?php echo $APP_URL; ?>/images/colaborador-lcnrs.png" alt="La ciudad nos regala sabores"></a>
+                        <a class="col-xs-6 col-sm-3 col-md-6 col-lg-4 lcnrs" href="https://www.facebook.com/LaCiudadNosRegalaSabores" target="_blank"><img src="<?php echo $APP_URL; ?>/images/colaborador-lcnrs.png" alt="La ciudad nos regala sabores"></a>
 
-						<a class="col-xs-6 col-sm-3 col-md-6 col-lg-4 laguna-fvet" href="https://www.facebook.com/elrenacerdelalaguna/" target="_blank"><img src="<?php echo $APP_URL; ?>/images/colaborador-laguna-fvet.png" alt="El Renacer de la Laguna - FVET - UBA"></a>
+                        <a class="col-xs-6 col-sm-3 col-md-6 col-lg-4 laguna-fvet" href="https://www.facebook.com/elrenacerdelalaguna/" target="_blank"><img src="<?php echo $APP_URL; ?>/images/colaborador-laguna-fvet.png" alt="El Renacer de la Laguna - FVET - UBA"></a>
 
-						<a class="col-xs-12 col-sm-3 col-md-12 col-lg-4 arn" href="https://www.facebook.com/AsociacionRiberaNorte/" target="_blank"><img src="<?php echo $APP_URL; ?>/images/colaborador-arn.png" alt="Asociación Ribera Norte"></a>	
-					</div>
-				</div>
+                        <a class="col-xs-12 col-sm-3 col-md-12 col-lg-4 arn" href="https://www.facebook.com/AsociacionRiberaNorte/" target="_blank"><img src="<?php echo $APP_URL; ?>/images/colaborador-arn.png" alt="Asociación Ribera Norte"></a>    
+                    </div>
+                </div>
 
-			</nav>
-		</div>
-		
-		<!-- mapa (container) -->
-		<div class="col-md-8 col-lg-9 full-height" id="mapa"> </div>
-	</div>
+            </nav>
+        </div>
+        
+        <!-- mapa (container) -->
+        <div class="col-md-8 col-lg-9 full-height" id="mapa"> </div>
+    </div>
 </div>
 
 <?php require_once('custom/scripts/modals.php') ?>
