050d0795571174b9ae876e1625901e0817426ede
Martín Simonyan <martin@simonyan.com.ar>
2023-03-01 14:18:03 -0300

    nuevo script de analytics 4

--- sitio/custom/scripts/funciones-js-footer.php
+++ sitio/custom/scripts/funciones-js-footer.php
@@ -6,7 +6,7 @@
   <?php endif; ?>
 
   // GOOGLE ANALYTICS
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  /*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
@@ -18,4 +18,17 @@
   <?php if (!empty($busqueda)) : ?>
     ga("send", "formulario posteado", "buscar", "arboles", <?php echo $total_registros_censo?> );
   <?php endif; ?>
+  -*
 </script>
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CFQ9D8W1T3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CFQ9D8W1T3');
+</script>
+
